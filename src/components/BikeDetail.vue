e9ab9a99706cfaf401dae6e14aad1364d8bf73c3
rkgupta-dev <rohitkrgupta333@gmail.com>
2024-10-31 00:51:46 +0530

    updates BikeDetail

--- src/components/BikeDetail.vue
+++ src/components/BikeDetail.vue
@@ -4,14 +4,59 @@
       <b-col cols="12" lg="6">
         <h2>{{ bike.name }}</h2>
         <b-img :src="bike.image" :alt="bike.name" fluid></b-img>
+        <small class="extra-small">
+          Images are for representation purposes only. Actual products delivered
+          may differ from the images displayed on the website.
+        </small>
       </b-col>
-      <b-col cols="12" lg="6">
-        <h5 class="font-weight-bold">Price: ₹ {{ bike.price }} /m</h5>
-        <h6 class="text-muted" style="text-decoration: line-through">
-          Original Price: ₹ {{ bike.originalPrice }}
-        </h6>
-        <h6 class="text-success">{{ calculateDiscount(bike) }}% OFF</h6>
-        <p><strong>Rating:</strong> {{ bike.rating }} Stars</p>
+      <b-col cols="12" lg="6" class="my-2">
+        <b-badge
+          variant="danger"
+          v-if="bike.highDemand"
+          class="mr-1 font-weight-bold h5 p-2"
+        >
+          High Demand
+        </b-badge>
+        <b-badge
+          variant="primary"
+          v-if="bike.highMileage"
+          class="mr-1 font-weight-bold h5 p-2"
+        >
+          High Mileage
+        </b-badge>
+        <b-badge
+          variant="success"
+          v-if="bike.bestSeller"
+          class="mr-1 font-weight-bold h5 p-2"
+        >
+          Bestseller
+        </b-badge>
+        <h6 class="text-muted">Monthly Rental</h6>
+        <div class="d-flex">
+          <h6 class="text-muted" style="text-decoration: line-through">
+            ₹ {{ bike.originalPrice }}
+          </h6>
+          <h6 class="text-success ml-2">
+            {{ calculateDiscount(bike) }}% off (limited offer)
+          </h6>
+        </div>
+        <h4 class="font-weight-bold">
+          Price: ₹ {{ bike.price }} /month <br />
+          <p></p>
+        </h4>
+
+        <div class="d-flex align-items-center">
+          <b-icon
+            v-for="i in 5"
+            :key="i"
+            icon="star-fill"
+            :variant="i <= Math.floor(bike.rating) ? 'warning' : 'secondary'"
+            class="mr-1"
+          ></b-icon>
+          <span class="text-muted ml-2"
+            >{{ bike.rating }} ({{ bike.ratingCount }})</span
+          >
+        </div>
         <p><strong>Status:</strong> {{ getStatusText(bike.status) }}</p>
         <p><strong>Description:</strong> {{ bike.description }}</p>
       </b-col>
@@ -42,6 +87,7 @@ export default {
           image:
             "https://on-track-jarvis.s3.ap-south-1.amazonaws.com/monthly-rental/rent-TVS-sport-bangalore-min.webp",
           rating: 4.8,
+          ratingCount: 634,
           price: 3999,
           originalPrice: 4500,
           highMileage: true,
@@ -54,6 +100,7 @@ export default {
           image:
             "https://on-track-jarvis.s3.ap-south-1.amazonaws.com/monthly-rental/rent-TVS-sport-bangalore-min.webp",
           rating: 4.6,
+          ratingCount: 545,
           price: 3000,
           originalPrice: 3500,
           highMileage: false,
@@ -67,6 +114,7 @@ export default {
           image:
             "https://on-track-jarvis.s3.ap-south-1.amazonaws.com/monthly-rental/rent-TVS-sport-bangalore-min.webp",
           rating: 4.6,
+          ratingCount: 234,
           price: 2999,
           originalPrice: 3499,
           highMileage: true,
@@ -80,6 +128,7 @@ export default {
           image:
             "https://on-track-jarvis.s3.ap-south-1.amazonaws.com/monthly-rental/rent-TVS-sport-bangalore-min.webp",
           rating: 4.6,
+          ratingCount: 469,
           price: 3499,
           originalPrice: 4000,
           highMileage: false,
@@ -104,3 +153,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.extra-small {
+  font-size: 0.75rem; /* Adjust this size as needed */
+}
+</style>
