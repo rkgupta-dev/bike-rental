02bff0d78fdba17dfebfdede196b9ac3744da9a5
rkgupta-dev <rohitkrgupta333@gmail.com>
2024-11-03 21:16:18 +0530

    updates

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -30,17 +30,55 @@
           </b-nav-item>
           <b-nav-item-dropdown right>
             <template #button-content> Hii, <em>Rohit Kr. Gupta</em> </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#" @click="toggleSidebar"
+              ><i class="fa-solid fa-user-cog text-primary"></i> Profile</b-dropdown-item
+            >
+            <b-dropdown-item href="#"> <i class="fa-solid fa-right-from-bracket text-danger"></i> Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
+    <b-sidebar v-model="isSidebarOpen" title="My Profile" shadow>
+      <b-list-group flush>
+      <b-list-group-item to="/rent" @click="navigateTo('bookings')">
+        <i class="fa-solid fa-calendar-check text-success"></i> Bookings
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('rentalHistory')">
+        <i class="fa-solid fa-clock-rotate-left text-warning"></i> Rental History
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('accountSettings')">
+        <i class="fa-solid fa-user-cog text-primary"></i> Account Settings
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('paymentMethods')">
+        <i class="fa-solid fa-credit-card text-success"></i> Payment Methods
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('notifications')">
+        <i class="fa-solid fa-bell text-danger"></i> Notifications
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('support')">
+        <i class="fa-solid fa-headset text-info"></i> Support
+      </b-list-group-item>
+      <b-list-group-item href="#" @click="navigateTo('logout')">
+        <i class="fa-solid fa-right-from-bracket text-danger"></i> Logout
+      </b-list-group-item>
+      </b-list-group>
+    </b-sidebar>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isSidebarOpen: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarOpen = true;
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -48,8 +86,10 @@ export default {};
   color: #f1f1f1; /* Change to your desired color */
   transition: color 0.3s ease;
 }
-
 .custom-navbar .nav-link:hover {
   color: #0f0c0e; /* Change on hover */
+}
+.b-list-group-item {
+  cursor: pointer;
 }
 </style>
